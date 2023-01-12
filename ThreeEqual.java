8a01ba3b615fcf58afb5a84655fcb3436a14f40c
Thatcher Auten <thatcherauten24@gmail.com>
2023-01-12 11:52:05 -0600

    cleaning up

--- ThreeEqual.java
+++ ThreeEqual.java
@@ -1,14 +1,17 @@
 public class ThreeEqual {
     public static void main(String[] args) {
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        int c = Integer.parseInt(args[2]);
-
-
-        if (a == b && b == c) {
-            System.out.println("equal");
+        if(args.length != 3){
+            System.out.println("Not enough arguments were provided");
         } else {
-            System.out.println("Not equal");
-        }
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            int c = Integer.parseInt(args[2]);
+
+            if (a == b && b == c) {
+                System.out.println("equal");
+            } else {
+                System.out.println("Not equal");
+            }
+        }    
     }
 }
