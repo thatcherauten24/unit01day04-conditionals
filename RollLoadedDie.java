c8820e11971a6f3a6959ec422b34264479aedf81
Thatcher Auten <thatcherauten24@gmail.com>
2023-01-13 11:24:08 -0600

    all of it

--- RollLoadedDie.java
+++ RollLoadedDie.java
@@ -1,5 +1,19 @@
 public class RollLoadedDie {
     public static void main(String[] args) {
+        double rn = Math.random();
+        if(rn<=0.125){
+            System.out.println(1);
+        } else if(rn > 0.125 && rn <= .250){
+            System.out.println(2);
+        } else if(rn > 0.250 && rn <= .375){
+            System.out.println(3);
+        } else if(rn > 0.375 && rn <= .500){
+            System.out.println(4);
+        } else if(rn > 0.500 && rn <= 0.625){
+            System.out.println(5);
+        } else {
+            System.out.println(6);
+        }
         
     }
 }
